6ab1e6ffd3643f6f0a9bbc2f0aebc421f24ee633
jcorreaw <juninhocorrea2014@gmail.com>
2023-07-21 11:18:40 -0300

    Mostrando Alert

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,15 +1,18 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 	
 	@FXML
 	private Button btTest;
 	
+	@FXML
 	public void onBtTestAction() {
-		System.out.println("Click");
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR);
 	}
 
 }
